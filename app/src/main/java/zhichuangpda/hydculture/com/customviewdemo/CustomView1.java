ee53ef4fc5b860480f67f0feba81879e578b3b49
httvc <923351919@qq.com>
2017-12-16 18:46:56 +0800

    提交

--- app/src/main/java/zhichuangpda/hydculture/com/customviewdemo/CustomView1.java
+++ app/src/main/java/zhichuangpda/hydculture/com/customviewdemo/CustomView1.java
@@ -3,6 +3,7 @@ package zhichuangpda.hydculture.com.customviewdemo;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -29,6 +30,7 @@ public class CustomView1 extends View{
     }
 
     Paint paint=new Paint();
+    Paint paint1=new Paint();
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -38,7 +40,13 @@ public class CustomView1 extends View{
         /**
          * RectF oval 作用：定义圆弧的形状和大小的范围
          */
-      //  RectF oval=new RectF();
+        float x = (getWidth() - getHeight() / 2) / 2;
+        float y = getHeight() / 4;
+        RectF oval = new RectF( x, y,
+                getWidth() - x, getHeight() - y);
       //  canvas.drawArc();
+        canvas.drawRect(oval,paint);
+        paint1.setARGB(00,55,55,55);
+        canvas.drawOval(oval,paint1);
     }
 }
